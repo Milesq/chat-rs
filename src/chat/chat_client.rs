573f97be5133fbbd74ec146b61b7debf1efa9cd0
Milesq <wmilosz88@gmail.com>
2020-03-16 13:53:26 +0100

    Add editorconfig

--- src/chat/chat_client.rs
+++ src/chat/chat_client.rs
@@ -1,4 +1,10 @@
-use std::net::IpAddr;
+use {
+    serde::Serialize,
+    std::{
+        io::{self, Read, Write},
+        net::{IpAddr, TcpStream},
+    },
+};
 
 pub struct ChatClient {
     pub name: String,
@@ -6,12 +12,26 @@ pub struct ChatClient {
     pub others_participants: Vec<String>,
 }
 
+#[derive(Serialize, Debug)]
+enum MsgType {
+    GetParticipants,
+    AddParticipant(String),
+}
+
 impl ChatClient {
-    pub fn new(name: String, server_ip: IpAddr) -> Self {
-        Self {
+    pub fn new(name: String, server_ip: IpAddr) -> io::Result<Self> {
+        Ok(Self {
             name,
             server_ip,
-            others_participants: vec![],
-        }
+            others_participants: Self::get_participants(server_ip)?,
+        })
+    }
+
+    fn get_participants(ip: IpAddr) -> io::Result<Vec<String>> {
+        let packet = bincode::serialize(&MsgType::GetParticipants)
+            .unwrap()
+            .bytes();
+        TcpStream::connect("192:80")?.write_all(packet);
+        Ok(vec![])
     }
 }
