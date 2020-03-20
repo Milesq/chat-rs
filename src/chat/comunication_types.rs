ac8d25343cccb300ca81fb40027a526edd995a15
Milesq <wmilosz88@gmail.com>
2020-03-20 15:41:54 +0100

    impl fmt::Display for WhatsUp

--- src/chat/comunication_types.rs
+++ src/chat/comunication_types.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum ReqType {
@@ -20,4 +21,17 @@ pub enum WhatsUp {
     NewParticipant(String),
     NewMessage((String, String)),
     ParticipantDisconected(String),
+}
+
+impl fmt::Display for WhatsUp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use WhatsUp::*;
+
+        let string = match self {
+            NewParticipant(name) => format!("{} has joined!", name),
+            NewMessage((user, msg)) => format!("[{}]: {}", user, msg),
+            ParticipantDisconected(name) => format!("{} has disconnected", name),
+        };
+        write!(f, "{}", string)
+    }
 }
